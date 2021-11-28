1c5c7609f99bd799c182ff5b210fad487ab691d7
Adi-semrisemri1 <>
2021-11-28 14:41:19 +0000

    Initial commit

---
+++ Materi 6/Associtive Array.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Belajar PHP</title>
+  </head>
+  <body>
+
+      <h1> Associative Array(Materi 6) </h1>
+
+    <?php 
+
+   
+    $data = ['nama' => 'Adineno',
+             'umur'=>21,
+             'sifat'=>'humble'];
+
+    $data ['nama'] = " Adi Semri Neno";
+    echo"Nama adalah".$data ['nama'];
+      
+      
+    ?>
+  </body>
+</html>
